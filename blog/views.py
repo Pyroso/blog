3b87b53f52fe7ef212fd882215bc8592cb54894b
C04H05 <c04h04@teb.edu.pl>
2024-03-22 13:22:02 +0100

    ListView nie jest zdefiniowany

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,17 @@ from blog.models import Post
 
 
 # Create your views here.
+'''
 def post_list(request):
     posts = Post.objects.all().filter(status='published')
     return render(request, "blog/post/list.html", {"posts": posts})
+'''
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post, slug=slug, status='published',
